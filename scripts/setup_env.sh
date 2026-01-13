1710df3d7c23097522841ddd5a98f7fa8adaa617
Kam-Tom <kamil8tomaszewski@gmail.com>
2026-01-13 19:12:21 +0100

    Add simple enviroment setup

---
+++ scripts/setup_env.sh
@@ -0,0 +1,4 @@
+pip install "numpy<2" mxnet opencv-python-headless matplotlib tqdm tensorboard scikit-learn kaggle && \
+export KAGGLE_USERNAME=kamtom && \
+export KAGGLE_KEY=e16928ff87b0bcf5b90e0a72af058372 && \
+echo "✅ Instalacja i autoryzacja gotowa!"
